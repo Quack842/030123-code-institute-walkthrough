e7926c36148d4a254ec7f3cf507ea6b067ba7580
Nicolene Marais <nici.marais.ie@gmail.com>
2023-01-05 09:40:57 +0000

    Added add_task to code

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -2,4 +2,15 @@ document.addEventListener('DOMContentLoaded', function() {
     // Sidenav Initialization
     var sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
+
+    // Datepicker Initialization
+    var datepicker = document.querySelectorAll('.datepicker');
+    M.Datepicker.init(datepicker, {
+      format: "dd-mmm-yyyy",
+      i18n: {done: "Select"}
+    });
+
+    // Select Initialization
+    var selects = document.querySelectorAll('select');
+    M.FormSelect.init(selects);
   });
